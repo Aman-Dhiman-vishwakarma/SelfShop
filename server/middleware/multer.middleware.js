96142a454f30d554d67e2507859b20d2efcd227a
Aman Dhiman vishwakarma <adhiman8439@gmail.com>
2025-04-25 15:31:10 +0530

    add file

--- server/middleware/multer.middleware.js
+++ server/middleware/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, "./upload");
+    return cb(null, "./server/upload");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
